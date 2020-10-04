be3aac793f188f09ffab8d1d22258380e28263cc
steven112163 <steven112163@gmail.com>
2020-10-04 20:47:04 +0800

    Implement matrix multiplication

--- HW1/regression.py
+++ HW1/regression.py
@@ -1,9 +1,46 @@
 import argparse
 import csv
 import pprint
+from typing import List, Tuple
 
 
-def compute_LU(mat=((2.0, -1.0, -2.0), (-4.0, 6.0, 3.0), (-4.0, -2.0, 8.0))):
+def compute_x(A_loc: List[List[float]], b_loc: List[float]) -> List[float]:
+    """
+    Compute vector x from matrix A, vector b and lambda
+    :param A_loc: matrix A
+    :param b_loc: vector b
+    :return: vector x
+    """
+
+
+def multiply_matrix(u: List[List[float]], v: List[List[float]]) -> List[List[float]] or List[float] or None:
+    """
+    Multiply two matrices
+    :param u: matrix u
+    :param v: matrix v
+    :return: result matrix or vector(number of rows is 1)
+    """
+    # Check dimension
+    if len(u[0]) != len(v):
+        return None
+
+    # Compute result
+    result = []
+    num_u_row = len(u)
+    num_v_col = len(v[0])
+    num_vec_multiplication = len(v)
+    for row in range(num_u_row):
+        partial_result = []
+        for col in range(num_v_col):
+            partial_result.append(sum(u[row][i] * v[i][col] for i in range(num_vec_multiplication)))
+        result.append(partial_result)
+    if len(result) == 1:
+        result = result[0]
+
+    return result
+
+
+def compute_LU(mat: List[List[float]]) -> Tuple[List[List[float]], List[List[float]]]:
     """
     Compute LU composition of mat
     :param mat: target matrix
@@ -55,12 +92,11 @@ if __name__ == '__main__':
 
     # Get points from the file
     reader = csv.reader(file)
-    points = []
-    for row in reader:
-        points.append([float(row[0]), float(row[1])])
+    points = [[float(row[0]), float(row[1])] for row in reader]
     file.close()
 
+    # Setup matrix A and vector b
+    A = [[points[row][0] ** t for t in range(n, -1, -1)] for row in range(len(points))]
+    b = [p[1] for p in points]
     pp = pprint.PrettyPrinter()
-    L_com, U_com = compute_LU()
-    pp.pprint(L_com)
-    pp.pprint(U_com)
+    pp.pprint(A)

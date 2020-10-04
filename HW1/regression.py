9527c00225032741e09760213192859750bc6b6b
steven112163 <steven112163@gmail.com>
2020-10-04 19:42:17 +0800

    Compute LU

--- HW1/regression.py
+++ HW1/regression.py
@@ -1,5 +1,31 @@
 import argparse
 import csv
+import pprint
+
+
+def compute_LU(mat=((2.0, -1.0, -2.0), (-4.0, 6.0, 3.0), (-4.0, -2.0, 8.0))):
+    """
+    Compute LU composition of mat
+    :param mat: target matrix
+    :return: L and U
+    """
+    # Get dimension of mat
+    dim = len(mat)
+
+    # Setup L and U
+    L = [[0.0 for _ in range(dim)] for _ in range(dim)]
+    U = [[0.0 for _ in range(dim)] for _ in range(dim)]
+    for dig in range(dim):
+        L[dig][dig] = 1.0
+
+    # Use Doolittle algorithm to perform LU composition
+    for i in range(dim):
+        for k in range(i, dim):
+            U[i][k] = mat[i][k] - sum(L[i][j] * U[j][k] for j in range(i))
+        for k in range(i + 1, dim):
+            L[k][i] = (mat[k][i] - sum(L[k][j] * U[j][i] for j in range(i))) / U[i][i]
+
+    return L, U
 
 
 def parse_arguments():
@@ -33,3 +59,8 @@ if __name__ == '__main__':
     for row in reader:
         points.append([float(row[0]), float(row[1])])
     file.close()
+
+    pp = pprint.PrettyPrinter()
+    L_com, U_com = compute_LU()
+    pp.pprint(L_com)
+    pp.pprint(U_com)

6bd791e04fc895978aa4eddee521513c6a1f036c
steven112163 <steven112163@gmail.com>
2020-11-01 17:25:41 +0800

    Plot the graph

--- HW3/bayesian_linear_regression.py
+++ HW3/bayesian_linear_regression.py
@@ -1,9 +1,10 @@
 import argparse
 import sys
-from typing import List, Tuple
+from typing import List, Tuple, Union
 from sequential_estimator import univariate_gaussian_data_generator
 from copy import deepcopy
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def polynomial_basis_linear_model_data_generator(basis: int, variance: float, omega: List) -> Tuple[float, float]:
@@ -107,7 +108,7 @@ def bayesian_linear_regression(basis: int, variance: float, omega: List, precisi
             fiftieth_covariance = deepcopy(posterior_covariance)
 
         # Break the loop if it converges
-        if np.linalg.norm(posterior_mean - prior_mean) < 0.00001:
+        if np.linalg.norm(posterior_mean - prior_mean) < 0.00001 and count > 50:
             break
 
         # Update prior
@@ -130,6 +131,88 @@ def create_design_matrix(x: float, basis: int) -> np.ndarray:
         design[0, i] = np.power(x, i)
 
     return design
+
+
+def draw_the_graph(weight_vector: List[float], variance: float, basis: int, points: List[List[float]],
+                   posterior_mean: float, posterior_covariance: np.ndarray, tenth_mean: float,
+                   tenth_covariance: np.ndarray, fiftieth_mean: float, fiftieth_covariance: np.ndarray) -> None:
+    """
+    Draw the graph
+    :param weight_vector: weight vector of the ground truth
+    :param variance: variance of polynomial basis linear model
+    :param basis: basis number of polynomial basis linear model
+    :param points: sample points
+    :param posterior_mean: converged posterior mean
+    :param posterior_covariance: converged posterior covariance
+    :param tenth_mean: tenth posterior mean
+    :param tenth_covariance: tenth posterior covariance
+    :param fiftieth_mean: fiftieth posterior mean
+    :param fiftieth_covariance: fiftieth posterior covariance
+    :return: None
+    """
+    x = np.linspace(-2.0, 2.0, 100)
+    points = np.transpose(points)
+
+    # Ground truth
+    plt.subplot(221)
+    plt.title('Ground truth')
+    f = np.poly1d(np.flip(weight_vector))
+    y = f(x)
+    draw_lines(x, y, variance)
+
+    # Predict result
+    plt.subplot(222)
+    plt.title('Predict result')
+    f = np.poly1d(np.flip(np.reshape(posterior_mean, basis)))
+    y = f(x)
+    var = np.zeros(100)
+    for i in range(100):
+        design = create_design_matrix(x[i], basis)
+        var[i] = variance + design.dot(posterior_covariance).dot(design.T)[0, 0]
+    plt.scatter(points[0], points[1], s=1)
+    draw_lines(x, y, var)
+
+    # After 10 times
+    plt.subplot(223)
+    plt.title('After 10 times')
+    f = np.poly1d(np.flip(np.reshape(tenth_mean, basis)))
+    y = f(x)
+    var = np.zeros(100)
+    for i in range(100):
+        design = create_design_matrix(x[i], basis)
+        var[i] = variance + design.dot(tenth_covariance).dot(design.T)[0, 0]
+    plt.scatter(points[0][:10], points[1][:10], s=1)
+    draw_lines(x, y, var)
+
+    # After 50 times
+    plt.subplot(224)
+    plt.title('After 50 times')
+    f = np.poly1d(np.flip(np.reshape(fiftieth_mean, basis)))
+    y = f(x)
+    var = np.zeros(100)
+    for i in range(100):
+        design = create_design_matrix(x[i], basis)
+        var[i] = variance + design.dot(fiftieth_covariance).dot(design.T)[0, 0]
+    plt.scatter(points[0][:50], points[1][:50], s=1)
+    draw_lines(x, y, var)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def draw_lines(x: np.ndarray, y: np.ndarray, variance: Union[float, np.ndarray]) -> None:
+    """
+    Draw predict line and two lines with variance
+    :param x: x coordinates of the points
+    :param y: y coordinates of the points
+    :param variance: y variance
+    :return: None
+    """
+    plt.plot(x, y, color='k')
+    plt.plot(x, y + variance, color='r')
+    plt.plot(x, y - variance, color='r')
+    plt.xlim(-2.0, 2.0)
+    plt.ylim(-15.0, 25.0)
 
 
 def info_log(log: str) -> None:
@@ -211,7 +294,9 @@ if __name__ == '__main__':
 
     if not mode:
         info_log('=== Bayesian linear regression ===')
-        bayesian_linear_regression(n, a, m, b)
+        sample_points, post_mean, post_covar, ten_mean, ten_covar, fifty_mean, fifty_covar = bayesian_linear_regression(
+            n, a, m, b)
+        draw_the_graph(m, a, n, sample_points, post_mean, post_covar, ten_mean, ten_covar, fifty_mean, fifty_covar)
     else:
         info_log('=== Polynomial basis linear model data generator ===')
         print(f'Data point: {polynomial_basis_linear_model_data_generator(n, a, m)}')

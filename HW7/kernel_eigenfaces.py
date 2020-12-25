71baf7ad24181ffc5d73e6a3dca4e6686ae8eb5e
steven112163 <steven112163@gmail.com>
2020-12-26 00:19:04 +0800

    Read images

--- HW7/kernel_eigenfaces.py
+++ HW7/kernel_eigenfaces.py
@@ -47,7 +47,7 @@ def parse_arguments() -> Namespace:
     """
     parser = ArgumentParser(description='Kernel eigenfaces')
     parser.add_argument('-i', '--image', help='Name of the directory containing images',
-                        default='data/Yale_Face_Database/')
+                        default='data/Yale_Face_Database')
     parser.add_argument('-algo', '--algorithm', help='Algorithm to be used, 0: PCA, 1: LDA', default=0,
                         type=check_int_range)
     parser.add_argument('-m', '--mode', help='Mode for PCA/LDA, 0: simple, 1: kernel', default=0, type=check_int_range)
@@ -63,7 +63,40 @@ if __name__ == '__main__':
     """
     # Get arguments
     args = parse_arguments()
-    name_of_dir = args.image
+    dir_name = args.image
     algo = args.algorithm
     m = args.mode
     verbosity = args.verbosity
+
+    # Read training images
+    info_log('=== Read training images ===')
+    train_images, train_labels = None, None
+    num_of_files = 0
+    with os.scandir(f'{dir_name}/Training') as directory:
+        # Get number of files
+        num_of_files = len([file for file in directory if file.is_file()])
+    with os.scandir(f'{dir_name}/Training') as directory:
+        train_labels = np.zeros(num_of_files, dtype=int)
+        # Images will be resized to 107 (rows) * 97 (cols)
+        train_images = np.zeros((num_of_files, 107 * 97))
+        for index, file in enumerate(directory):
+            if file.path.endswith('.pgm') and file.is_file():
+                face = np.asarray(Image.open(file.path).resize((97, 107))).reshape(1, -1)
+                train_images[index, :] = face
+                train_labels[index] = int(file.name[7:9])
+
+    # Read testing images
+    info_log('=== Read testing images ===')
+    test_images, test_labels = None, None
+    with os.scandir(f'{dir_name}/Testing') as directory:
+        # Get number of files
+        num_of_files = len([file for file in directory if file.is_file()])
+    with os.scandir(f'{dir_name}/Testing') as directory:
+        test_labels = np.zeros(num_of_files, dtype=int)
+        # Images will be resized to 107 (rows) * 97 (cols)
+        test_images = np.zeros((num_of_files, 107 * 97))
+        for index, file in enumerate(directory):
+            if file.path.endswith('.pgm') and file.is_file():
+                face = np.asarray(Image.open(file.path).resize((97, 107))).reshape(1, -1)
+                test_images[index, :] = face
+                test_labels[index] = int(file.name[7:9])

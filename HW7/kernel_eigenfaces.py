cd9d5f78776a12d550ea4b4d7a50c29ea184976e
steven112163 <steven112163@gmail.com>
2020-12-27 01:11:31 +0800

    Add simple lda but with bugs

--- HW7/kernel_eigenfaces.py
+++ HW7/kernel_eigenfaces.py
@@ -34,44 +34,16 @@ def principal_components_analysis(training_images: np.ndarray, training_labels: 
         info_log(f'=== {"RBF" if kernel_type else "Linear"} kernel PCA ===')
         matrix = kernel_pca(training_images, kernel_type, gamma)
 
-    # Compute eigenvalues and eigenvectors
-    info_log('=== Calculate eigenvalues and eigenvectors ===')
-    eigenvalues, eigenvectors = np.linalg.eig(matrix)
-
-    # Get 25 first largest eigenvectors
-    info_log('=== Get 25 first largest eigenvectors ===')
-    target_idx = np.argsort(eigenvalues)[::-1][:25]
-    target_eigenvectors = eigenvectors[:, target_idx].real
+    # Find 25 first largest eigenvectors
+    target_eigenvectors = find_target_eigenvectors(matrix)
 
     # Transform eigenvectors into eigenfaces
     info_log('=== Transform eigenvectors into eigenfaces ===')
-    eigenfaces = target_eigenvectors.T.reshape((25, 107, 97))
-    fig = plt.figure(1)
-    fig.canvas.set_window_title('Eigenfaces')
-    for idx in range(25):
-        plt.subplot(5, 5, idx + 1)
-        plt.axis('off')
-        plt.imshow(eigenfaces[idx, :, :], cmap='gray')
+    transform_eigenvectors_into_faces(target_eigenvectors, 0)
 
     # Randomly reconstruct 10 eigenfaces
     info_log('=== Reconstruct 10 faces ===')
-    reconstructed_images = np.zeros((10, 107 * 97))
-    choice = np.random.choice(num_of_training, 10)
-    for idx in range(10):
-        reconstructed_images[idx, :] = training_images[choice[idx], :].dot(target_eigenvectors).dot(
-            target_eigenvectors.T)
-    fig = plt.figure(2)
-    fig.canvas.set_window_title('Reconstructed faces')
-    for idx in range(10):
-        # Original image
-        plt.subplot(10, 2, idx * 2 + 1)
-        plt.axis('off')
-        plt.imshow(training_images[choice[idx], :].reshape((107, 97)), cmap='gray')
-
-        # Reconstructed image
-        plt.subplot(10, 2, idx * 2 + 2)
-        plt.axis('off')
-        plt.imshow(reconstructed_images[idx, :].reshape((107, 97)), cmap='gray')
+    construct_faces(num_of_training, training_images, target_eigenvectors)
 
     # Classify
     info_log('=== Classify ===')
@@ -134,6 +106,146 @@ def kernel_pca(training_images: np.ndarray, kernel_type: int, gamma: float) -> n
     return matrix
 
 
+def linear_discriminative_analysis(training_images: np.ndarray, training_labels: np.ndarray, testing_images: np.ndarray,
+                                   testing_labels: np.ndarray, mode: int, k_neighbors: int, kernel_type: int,
+                                   gamma: float) -> None:
+    """
+    Linear discriminative analysis (Fisher's discriminative analysis)
+    :param training_images: training images
+    :param training_labels: training labels
+    :param testing_images: testing images
+    :param testing_labels: testing labels
+    :param mode: 0: simple, 1: kernel
+    :param k_neighbors: number of nearest neighbors to decide classification
+    :param kernel_type: 0 for linear, 1 for RBF
+    :param gamma: gamma of RBF
+    :return: None
+    """
+    # Get number of classes and number of training images
+    _, num_of_each_class = np.unique(training_labels, return_counts=True)
+    num_of_training = len(training_images)
+
+    if not mode:
+        # Simple LDA
+        info_log('=== Simple LDA ===')
+        matrix = simple_lda(num_of_each_class, training_images, training_labels)
+
+    # Find 25 first largest eigenvectors
+    target_eigenvectors = find_target_eigenvectors(matrix)
+
+    # Transform eigenvectors into fisherfaces
+    info_log('=== Transform eigenvectors into fisherfaces ===')
+    transform_eigenvectors_into_faces(target_eigenvectors, 1)
+
+    # Randomly reconstruct 10 eigenfaces
+    info_log('=== Reconstruct 10 faces ===')
+    construct_faces(num_of_training, training_images, target_eigenvectors)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def simple_lda(num_of_each_class: np.ndarray, training_images: np.ndarray, training_labels: np.ndarray) -> np.ndarray:
+    """
+    Simple LDA
+    :param num_of_each_class: number of elements in each class
+    :param training_images: training images
+    :param training_labels: training labels
+    :return: matrix
+    """
+    # Get overall mean
+    info_log('=== Calculate overall mean ===')
+    overall_mean = np.mean(training_images, axis=0)
+
+    # Get mean of each class
+    info_log('=== Calculate mean of each class ===')
+    num_of_classes = len(num_of_each_class)
+    class_mean = np.zeros((num_of_classes, 107 * 97))
+    for label in range(num_of_classes):
+        class_mean[label, :] = np.mean(training_images[training_labels == label + 1], axis=0)
+
+    # Get between-class scatter
+    info_log('=== Calculate between-class scatter ===')
+    scatter_b = np.zeros((107 * 97, 107 * 97), dtype=float)
+    for idx, num in enumerate(num_of_each_class):
+        difference = (class_mean[idx] - overall_mean).reshape((107 * 97, 1))
+        scatter_b += num * difference.dot(difference.T)
+
+    # Get within-class scatter
+    info_log('=== Calculate within-class scatter ===')
+    scatter_w = np.zeros((107 * 97, 107 * 97), dtype=float)
+    for idx, mean in enumerate(class_mean):
+        difference = training_images[training_labels == idx + 1] - mean
+        scatter_w += difference.T.dot(difference)
+
+    # Get Sw^(-1) * Sb
+    info_log('=== Calculate inv(within-class)*between-class ===')
+    matrix = np.linalg.pinv(scatter_w).dot(scatter_b)
+
+    return matrix
+
+
+def find_target_eigenvectors(matrix: np.ndarray) -> np.ndarray:
+    """
+    Find 25 first largest eigenvectors
+    :param matrix: given matrix
+    :return: 25 first largest eigenvectors
+    """
+    # Compute eigenvalues and eigenvectors
+    info_log('=== Calculate eigenvalues and eigenvectors ===')
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+
+    # Get 25 first largest eigenvectors
+    info_log('=== Get 25 first largest eigenvectors ===')
+    target_idx = np.argsort(eigenvalues)[::-1][:25]
+    target_eigenvectors = eigenvectors[:, target_idx].real
+
+    return target_eigenvectors
+
+
+def transform_eigenvectors_into_faces(target_eigenvectors: np.ndarray, pca_or_lda: int) -> None:
+    """
+    Transform eigenvectors into eigenfaces/fisherfaces
+    :param target_eigenvectors: 25 first largest eigenvectors
+    :param pca_or_lda: 0 for pca, 1 for lda
+    :return: None
+    """
+    faces = target_eigenvectors.T.reshape((25, 107, 97))
+    fig = plt.figure(1)
+    fig.canvas.set_window_title(f'{"Eigenfaces" if not pca_or_lda else "Fisherfaces"}')
+    for idx in range(25):
+        plt.subplot(5, 5, idx + 1)
+        plt.axis('off')
+        plt.imshow(faces[idx, :, :], cmap='gray')
+
+
+def construct_faces(num_of_images: int, training_images: np.ndarray, target_eigenvectors: np.ndarray) -> None:
+    """
+    Reconstruct faces from eigenfaces/fisherfaces
+    :param num_of_images: number of images
+    :param training_images: training images
+    :param target_eigenvectors: 25 first largest eigenvectors
+    :return: None
+    """
+    reconstructed_images = np.zeros((10, 107 * 97))
+    choice = np.random.choice(num_of_images, 10)
+    for idx in range(10):
+        reconstructed_images[idx, :] = training_images[choice[idx], :].dot(target_eigenvectors).dot(
+            target_eigenvectors.T)
+    fig = plt.figure(2)
+    fig.canvas.set_window_title('Reconstructed faces')
+    for idx in range(10):
+        # Original image
+        plt.subplot(10, 2, idx * 2 + 1)
+        plt.axis('off')
+        plt.imshow(training_images[choice[idx], :].reshape((107, 97)), cmap='gray')
+
+        # Reconstructed image
+        plt.subplot(10, 2, idx * 2 + 2)
+        plt.axis('off')
+        plt.imshow(reconstructed_images[idx, :].reshape((107, 97)), cmap='gray')
+
+
 def decorrelate(num_of_images: int, images: np.ndarray, eigenvectors: np.ndarray) -> np.ndarray:
     """
     Decorrelate original images into components space
@@ -147,19 +259,6 @@ def decorrelate(num_of_images: int, images: np.ndarray, eigenvectors: np.ndarray
         decorrelated_images[idx, :] = image.dot(eigenvectors)
 
     return decorrelated_images
-
-
-def linear_discriminative_analysis(training_images: np.ndarray, training_labels: np.ndarray, testing_images: np.ndarray,
-                                   testing_labels: np.ndarray, mode: int) -> None:
-    """
-    Linear discriminative analysis (Fisher's discriminative analysis)
-    :param training_images: training images
-    :param training_labels: training labels
-    :param testing_images: testing images
-    :param testing_labels: testing labels
-    :param mode: 0: simple, 1: kernel
-    :return: None
-    """
 
 
 def info_log(log: str) -> None:
@@ -210,7 +309,7 @@ def parse_arguments() -> Namespace:
                         type=int)
     parser.add_argument('-ker', '--kernel', help='Kernel type, 0 for linear, 1 for RBF', default=0,
                         type=check_int_range)
-    parser.add_argument('-g', '--gamma', help='Gamma of RBF', default=0.0001, type=float)
+    parser.add_argument('-g', '--gamma', help='Gamma of RBF', default=0.000001, type=float)
     parser.add_argument('-v', '--verbosity', help='verbosity level (0-1)', default=0, type=check_int_range)
 
     return parser.parse_args()
@@ -271,4 +370,4 @@ if __name__ == '__main__':
     else:
         # LDA
         info_log('=== Linear discriminative analysis ===')
-        linear_discriminative_analysis(train_images, train_labels, test_images, test_labels, m)
+        linear_discriminative_analysis(train_images, train_labels, test_images, test_labels, m, k, ker_type, g)

d1d6077bab484da0044c3ffa7f36396b1efde25f
steven112163 <steven112163@gmail.com>
2020-12-26 14:13:43 +0800

    Add simple PCA

--- HW7/kernel_eigenfaces.py
+++ HW7/kernel_eigenfaces.py
@@ -7,6 +7,77 @@ from PIL import Image
 from scipy.spatial.distance import cdist
 
 
+def principal_components_analysis(training_images: np.ndarray, training_labels: np.ndarray, testing_images: np.ndarray,
+                                  testing_labels: np.ndarray, mode: int) -> None:
+    """
+    Principal components analysis
+    :param training_images: training images
+    :param training_labels: training labels
+    :param testing_images: testing images
+    :param testing_labels: testing labels
+    :param mode: 0: simple, 1: kernel
+    :return: None
+    """
+    # Get number of images
+    num_of_images = len(training_images)
+
+    if not mode:
+        # Simple PCA
+        info_log('=== Simple PCA ===')
+        matrix = simple_pca(num_of_images, training_images)
+    else:
+        # Kernel PCA
+        info_log('=== Kernel PCA ===')
+
+    # Compute eigenvalues and eigenvectors
+    info_log('=== Calculate eigenvalues and eigenvectors ===')
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+
+    # Get 25 first largest eigenvectors
+    info_log('=== Get 25 first largest eigenvectors ===')
+    target_idx = np.argsort(eigenvalues)[::-1][:25]
+    target_eigenvectors = eigenvectors[:, target_idx].real
+
+    # Transform eigenvectors into eigenfaces
+    eigenfaces = target_eigenvectors.T.reshape((25, 107, 97))
+    plt.figure(1)
+    for idx in range(25):
+        plt.subplot(5, 5, idx + 1)
+        plt.axis('off')
+        plt.imshow(eigenfaces[idx, :, :], cmap='gray')
+    plt.show()
+
+
+def simple_pca(num_of_images: int, training_images: np.ndarray) -> np.ndarray:
+    """
+    Simple PCA
+    :param num_of_images: number of training images
+    :param training_images: training images
+    :return: covariance
+    """
+    # Compute covariance
+    training_images_transposed = training_images.T
+    mean = np.mean(training_images_transposed, axis=1)
+    mean = np.tile(mean.T, (num_of_images, 1)).T
+    difference = training_images_transposed - mean
+    covariance = difference.dot(difference.T) / num_of_images
+
+    return covariance
+
+
+def linear_discriminative_analysis(training_images: np.ndarray, training_labels: np.ndarray, testing_images: np.ndarray,
+                                   testing_labels: np.ndarray, mode: int) -> None:
+    """
+    Linear discriminative analysis (Fisher's discriminative analysis)
+    :param training_images: training images
+    :param training_labels: training labels
+    :param testing_images: testing images
+    :param testing_labels: testing labels
+    :param mode: 0: simple, 1: kernel
+    :return: None
+    """
+
+
 def info_log(log: str) -> None:
     """
     Print information log
@@ -59,7 +130,7 @@ def parse_arguments() -> Namespace:
 if __name__ == '__main__':
     """
     Main function
-    command: python3 kernel_eigenfaces.py
+    command: python3 kernel_eigenfaces.py [-i name_of_directory] [-algo (0-1)] [-m (0-1)] [-v (0-1)]
     """
     # Get arguments
     args = parse_arguments()
@@ -100,3 +171,12 @@ if __name__ == '__main__':
                 face = np.asarray(Image.open(file.path).resize((97, 107))).reshape(1, -1)
                 test_images[index, :] = face
                 test_labels[index] = int(file.name[7:9])
+
+    if not algo:
+        # PCA
+        info_log('=== Principal components analysis ===')
+        principal_components_analysis(train_images, train_labels, test_images, test_labels, m)
+    else:
+        # LDA
+        info_log('=== Linear discriminative analysis ===')
+        linear_discriminative_analysis(train_images, train_labels, test_images, test_labels, m)
